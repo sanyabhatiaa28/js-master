925f2eaba82bd03a400e0c59525c15ecdc99698b
sanyabhatiaa28 <sanyabhatiaa@gmail.com>
2026-05-17 17:45:59 +0000

    learned these things

---
+++ basics/conversionOperations.js
@@ -0,0 +1,20 @@
+// let score=33
+// console.log(typeof score);
+// console.log(typeof (score)); //both ways are correct to check type
+//both print number
+// let score ="33" prints string
+
+//let score="33abx" 
+//let valInInteger=Number(score) //prints number as datatype
+//but if we print score now it will give NaN not a number.
+//this is tricky part here
+// "33bx" ko number me convert krenge ho jayega but print pe NaN ayega
+//dnt relay on numbers that much.
+
+
+// let score =null
+// console.log(typeof score); object print
+//let inInteger=Number(score) if we checkk type its number if print gives 0.
+//undefined case me bhi NaN ayega
+//boolean me true pe 1 false pe 0
+//string like "sanya" number me print me NaN ayega. type bhale hi as per changes dikhe.
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,25 +1,38 @@
-// "use strict"; treat all js code as newer version
-//whole js is in browser, there alert(3+3) works as pop up.
-//but not in node. it has diff syntax here
-//we using node.js not browser.
-//code must be readable.
+// // "use strict"; treat all js code as newer version
+// //whole js is in browser, there alert(3+3) works as pop up.
+// //but not in node. it has diff syntax here
+// //we using node.js not browser.
+// //code must be readable.
  
-//number data type  
-let age=28
-//string data type
-let name="Sanya" // prefer "" over '' here
-//boolean data type
-let isGirl= true
-//null=> stand alone value(empty value),,,,can assign.
-//undefined=> declared but value not assigned yet.
-//big int
-//symbol =>for uniqueness
-/*
-above ones
-are primitive.
-*/
+// //number data type  
+// let age=28
+// //string data type
+// let name="Sanya" // prefer "" over '' here
+// //boolean data type
+// let isGirl= true
+// //null=> stand alone value(empty value),,,,can assign.
+// //undefined=> declared but value not assigned yet.
+// //big int
+// //symbol =>for uniqueness
+// /*
+// above ones
+// are primitive.
+// */
 
-//objects
-console.log(typeof "san"); //string print.
-console.log(typeof null); //object as output 
-console.log(typeof undefined); //undefined as output
+// // console.log(typeof (37)); //also correct way to check type. this is method. way to check.
+// // console.log(typeof "san"); //string print.
+// // console.log(typeof null); //object as output 
+// // console.log(typeof undefined); //undefined as output
+
+
+// // let isloggedin=1/0  if "" covert == false if "sanya"==> true
+// // let boolisloggedin=Boolean(isloggedin)
+// // console.log(boolisloggedin);  prints true/false
+
+
+
+// let num = 33
+// let strnum = String(num)
+
+// console.log(strnum);  //33
+// console.log(typeof strnum); //string
