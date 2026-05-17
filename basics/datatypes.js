0093bde3caed4cf1093bf6f5764cc4dd84e2eca7
sanyabhatiaa28 <sanyabhatiaa@gmail.com>
2026-05-17 17:05:25 +0000

    learned datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+// "use strict"; treat all js code as newer version
+//whole js is in browser, there alert(3+3) works as pop up.
+//but not in node. it has diff syntax here
+//we using node.js not browser.
+//code must be readable.
+ 
+//number data type  
+let age=28
+//string data type
+let name="Sanya" // prefer "" over '' here
+//boolean data type
+let isGirl= true
+//null=> stand alone value(empty value),,,,can assign.
+//undefined=> declared but value not assigned yet.
+//big int
+//symbol =>for uniqueness
+/*
+above ones
+are primitive.
+*/
+
+//objects
+console.log(typeof "san"); //string print.
+console.log(typeof null); //object as output 
+console.log(typeof undefined); //undefined as output
