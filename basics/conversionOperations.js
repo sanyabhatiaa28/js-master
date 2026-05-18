af09f38f40631ebeaa44268d63654999e1796a37
sanyabhatiaa28 <sanyabhatiaa@gmail.com>
2026-05-18 05:35:53 +0000

    done

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -18,3 +18,42 @@
 //undefined case me bhi NaN ayega
 //boolean me true pe 1 false pe 0
 //string like "sanya" number me print me NaN ayega. type bhale hi as per changes dikhe.
+//****************operations************************
+// let value=3
+// let negval=-value
+// console.log(negval); print -3
+
+// console.log(2+2,2-2,2*2,2**2,2**,2/3,2%3); **=>power
+// let str1="hello"
+// let str2=" sanya"  //we took gap before sanya since we addin both
+// let str3= str1+str2
+// console.log(str3); //concatinated as hello sanya
+//  console.log("1"+2);=>12
+//  console.log(1+"2");=>12
+//  console.log("1"+2+2)=>122//str 1st so treated as str as in whole
+//  console.log(1+2+"2")=>32  if str is 1st whole treated as str. if str last then whole treated as 1st one like here num.
+ //use () for these numerical things.
+
+
+
+//these tricky conversion avoids.
+// console.log(true); prints true
+// console.log(+true);
+// => 1 conversion ho jata
+// also true+ gives error
+// console.log(+""); output is 0;
+
+// let num1,num2,num3
+// num1=num2=num3=2+3 why to do this avoid.
+
+// let gamecounter=100
+// ++gamecounter;
+// console.log(gamecounter); 101 o/p
+
+// ++x => increment and return value after incrementing
+// x++ => increment and return value before incrementing
+    
+
+
+
+
