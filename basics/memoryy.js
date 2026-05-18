afabdb1c3f3d12e78fe06da3d477a955baece107
sanyabhatiaa28 <sanyabhatiaa@gmail.com>
2026-05-18 08:40:54 +0000

    dn

---
+++ basics/memoryy.js
@@ -0,0 +1,26 @@
+//memory
+/*
+stack memory
+for primitive
+copy milti variable ki usme changes hote
+
+*/
+/*
+heap memory
+for non primitives
+reference milta changes original me dikhenge.
+ */
+
+
+// let name="sanya"
+// let anothername= name
+// console.log(name); sanya
+// anothername="gungun"
+// console.log(anothername); gungun
+// let user={
+//     email:"san@123",
+//     upi:"123@ybl"
+// }
+// let user2=user
+// user2.email="3333@gmail"
+// console.table([user,user2]) // value change hogye user k liye bhi when we tried to use it in user2. user2 changes se original bhi change hogya.
